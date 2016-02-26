b9728a86c1317f8d5e9574ba1e66d99e0f8d9cd7
Danilo Maia Rodrigues <zidalo@gmail.com>
2016-02-26 14:47:19 -0300

    Categories API in admin

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,35 @@
 class Category < ActiveRecord::Base
   belongs_to :production_order
-  # accepts_nested_attributes_for :sub_category, allow_destroy: true
+
+  def api_id_enum
+    Api::Group.select_formatted
+  end
+
+  def subcategory_api_id_enum
+    if self.api_id.nil?
+      []
+    else
+      Api::Subgroup.select_formatted(self.api_id)
+    end
+  end
+
+  rails_admin do
+    list do
+      configure :api_id do
+        hide
+      end
+      configure :subcategory_api_id do
+        hide
+      end
+    end
+
+    show do
+      configure :api_id do
+        hide
+      end
+      configure :subcategory_api_id do
+        hide
+      end
+    end
+  end
 end

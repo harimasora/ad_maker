235ed51d6c6c117fe99cee0963fc556f9da1f1a0
Danilo Maia Rodrigues <zidalo@gmail.com>
2016-03-02 07:14:35 -0300

    corrected null pointer on subcategory api

--- app/controllers/api/subgroups_controller.rb
+++ app/controllers/api/subgroups_controller.rb
@@ -1,18 +1,18 @@
 class Api::SubgroupsController < ApplicationController
   def index
 
-    if params[:category][:api_id].nil?
-      if params[:production_order][:categories_attributes].nil?
+    if params[:category].try(:api_id).nil?
+      if params[:production_order].try(:categories_attributes).nil?
         @subgroups = []
       else
-        select_field_hash = params[:production_order][:categories_attributes]
+        select_field_hash = params[:production_order].try(:categories_attributes)
         keys = select_field_hash.keys
         select_field = select_field_hash[keys.first]
         api_id = select_field[:api_id]
         @subgroups = Api::Subgroup.select_formatted(api_id)
       end
     else
-      @subgroups = Api::Subgroup.select_formatted(params[:category][:api_id])
+      @subgroups = Api::Subgroup.select_formatted(params[:category].try(:api_id))
     end
 
     render :json => @subgroups
